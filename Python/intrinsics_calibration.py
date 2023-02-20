0296637e139382aae1d0daac781f57e6c724d252
Saad Alhasan <104773395+Saadalh@users.noreply.github.com>
2023-02-20 13:36:57 +0100

    Update intrinsics_calibration.py

--- Python/intrinsics_calibration.py
+++ Python/intrinsics_calibration.py
@@ -7,6 +7,10 @@ import os
 # Define the size of the ArUco markers (in this case, 6x6 bits and 100 pixels)
 marker_size = 100
 
+# Define the number of rows and columns in the gridboard
+gridboard_rows = 5
+gridboard_cols = 7
+
 # Define the dictionary of ArUco markers to be used (in this case, the standard DICT_6X6_250 dictionary)
 dictionary = aruco.Dictionary_get(aruco.DICT_6X6_250)
 
@@ -15,7 +19,7 @@ obj_points = []  # 3D points in real world space
 img_points = []  # 2D points in image plane
 
 # Define the grid of ArUco markers to be used for calibration
-gridboard = aruco.GridBoard_create(5, 7, marker_size, 1, dictionary)
+gridboard = aruco.GridBoard_create(gridboard_rows, gridboard_cols, marker_size, 1, dictionary)
 
 # Capture a set of calibration images of the calibration target from different viewpoints
 calibration_images = glob.glob('calibration_images/*.jpg')
@@ -29,13 +33,15 @@ for image_file in calibration_images:
     corners, ids, _ = aruco.detectMarkers(gray, dictionary)
     
     # If all the markers are detected, compute the 3D and 2D points
-    if len(corners) == 35:
-        _, rvecs, tvecs = aruco.estimatePoseBoard(corners, ids, gridboard, camera_matrix, distortion_coefficients)
+    if len(corners) == gridboard_rows * gridboard_cols:
+        objp = gridboard.getGridPoints()
         img_points.append(corners)
-        obj_points.append(gridboard.objPoints)
-    else:
-        # Remove the image file if it does not have the expected number of markers
-        os.remove(image_file)
+        obj_points.append(objp)
+
+        # Draw the detected markers on the calibration image and display it
+        img = cv2.aruco.drawDetectedMarkers(img, corners)
+        cv2.imshow('calibration image', img)
+        cv2.waitKey(0)
 
 # Perform camera calibration using the observed 2D and corresponding 3D points
 retval, camera_matrix, distortion_coefficients, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
@@ -43,3 +49,6 @@ retval, camera_matrix, distortion_coefficients, rvecs, tvecs = cv2.calibrateCame
 # Print the intrinsic camera parameters
 print('Camera matrix:\n', camera_matrix)
 print('Distortion coefficients:', distortion_coefficients)
+
+# Save the camera matrix and distortion coefficients to a file
+np.savez('calibration.npz', mtx=camera_matrix, dist=distortion_coefficients)
