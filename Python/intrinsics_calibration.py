d8b9a1544f74437d61af4e53ab6efa66859397ed
Saad Alhasan <104773395+Saadalh@users.noreply.github.com>
2023-02-20 19:25:30 +0100

    Update intrinsics_calibration.py

--- Python/intrinsics_calibration.py
+++ Python/intrinsics_calibration.py
@@ -1,28 +1,29 @@
 import numpy as np
 import cv2
 import glob
-import cv2.aruco as aruco
+from cv2 import aruco
 import os
 
 # Define the size of the ArUco markers (in this case, 6x6 bits and 100 pixels)
 marker_size = 100
 
 # Define the number of rows and columns in the gridboard
-gridboard_rows = 5
-gridboard_cols = 7
+gridboard_rows = 4
+gridboard_cols = 5
 
 # Define the dictionary of ArUco markers to be used (in this case, the standard DICT_6X6_250 dictionary)
-dictionary = aruco.Dictionary_get(aruco.DICT_6X6_250)
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_1000)
+parameters =  cv2.aruco.DetectorParameters()
 
 # Initialize arrays to store the observed 2D points and corresponding 3D points
 obj_points = []  # 3D points in real world space
 img_points = []  # 2D points in image plane
 
 # Define the grid of ArUco markers to be used for calibration
-gridboard = aruco.GridBoard_create(gridboard_rows, gridboard_cols, marker_size, 1, dictionary)
+gridboard = aruco.GridBoard((gridboard_rows, gridboard_cols), marker_size, 1, dictionary)
 
 # Capture a set of calibration images of the calibration target from different viewpoints
-calibration_images = glob.glob('calibration_images/*.jpg')
+calibration_images = glob.glob('/home/ubuntu/Python/Multi-Sensor_Calibrartion/aruco_data/*.jpg')
 
 for image_file in calibration_images:
     # Load the image and convert to grayscale
@@ -30,21 +31,23 @@ for image_file in calibration_images:
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     # Detect the ArUco markers in the image
-    corners, ids, _ = aruco.detectMarkers(gray, dictionary)
+    detector = aruco.ArucoDetector(dictionary, parameters)
+    corners, ids, _ = detector.detectMarkers(gray)
     
     # If all the markers are detected, compute the 3D and 2D points
     if len(corners) == gridboard_rows * gridboard_cols:
-        objp = gridboard.getGridPoints()
+        objp = gridboard.getObjPoints()
         img_points.append(corners)
         obj_points.append(objp)
 
         # Draw the detected markers on the calibration image and display it
-        img = cv2.aruco.drawDetectedMarkers(img, corners)
+        color = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        img = cv2.aruco.drawDetectedMarkers(color, corners, borderColor = (0,255,0))
         cv2.imshow('calibration image', img)
-        cv2.waitKey(0)
+        cv2.waitKey(500)
 
 # Perform camera calibration using the observed 2D and corresponding 3D points
-retval, camera_matrix, distortion_coefficients, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+retval, camera_matrix, distortion_coefficients, rvecs, tvecs = cv2.calibrateCamera(gridboard.getObjPoints(), corners, gray.shape[::-1], None, None)
 
 # Print the intrinsic camera parameters
 print('Camera matrix:\n', camera_matrix)
