42a1b589c77c2c464070f39030b942963906e62b
alhasan.saad@gmail.com <Saadalh>
2023-03-06 14:16:01 +0100

    charuco calib code modified based on v4.6

--- intrinsics_calibration/src/undistort.py
+++ intrinsics_calibration/src/undistort.py
@@ -8,15 +8,17 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True, help="path to the distorted input image.")
 args = vars(ap.parse_args())
 
-# Load the image
-img = cv.imread(args["input"])
-
 # Load the npz file
 npzfile = np.load(f'{dir_path}/../charuco_calibration_values.npz')
 
 # Extract the matrix values
 intrinsic_matrix = npzfile['mtx']
 distortion_coeffs = npzfile['dist']
+
+# Load the image
+img = cv.imread(args["input"])
+h,  w = img.shape[:2]
+newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(intrinsic_matrix, distortion_coeffs, (w,h), 1, (w,h))
 
 print("mtx:")
 print(intrinsic_matrix)
@@ -33,6 +35,10 @@ distortion_coeffs = np.array([distortion_coeffs[0][0], distortion_coeffs[0][1], 
 
 # Undistort the image
 undistorted_img = cv.undistort(img, intrinsic_matrix, distortion_coeffs)
+
+# crop the image
+x, y, w, h = roi
+undistorted_img = undistorted_img[y:y+h, x:x+w]
 
 # Display the undistorted image
 cv.imshow('Distorted Image', img)
