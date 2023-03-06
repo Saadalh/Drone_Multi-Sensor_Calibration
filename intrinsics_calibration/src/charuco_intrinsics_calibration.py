555331a549fa80e384b6353bc64d1e48dee78dee
alhasan.saad@gmail.com <Saadalh>
2023-03-06 14:27:19 +0100

    modifications

--- intrinsics_calibration/src/charuco_intrinsics_calibration.py
+++ intrinsics_calibration/src/charuco_intrinsics_calibration.py
@@ -4,37 +4,29 @@ from cv2 import aruco
 import glob
 import os
 
-
 # ChAruco board variables
-CHARUCOBOARD_ROWCOUNT = 7
-CHARUCOBOARD_COLCOUNT = 5 
+CHESSBOARD_X_COUNT = 7
+CHESSBOARD_Y_COUNT = 5 
 squareLength = 0.04
-squareLength_mm = 40
 markerLength = 0.031
-ARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-ARUCO_PARAM =  cv2.aruco.DetectorParameters()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_6X6_250)
 chessboardSize = (6,4)
 frameSize = (640,480)
 
 # Create constants to be passed into OpenCV and Aruco methods
-CHARUCO_BOARD = aruco.CharucoBoard(
-        (CHARUCOBOARD_COLCOUNT,CHARUCOBOARD_ROWCOUNT),
+CHARUCO_BOARD = aruco.CharucoBoard.create(
+        CHESSBOARD_X_COUNT,
+        CHESSBOARD_Y_COUNT,
         squareLength,
         markerLength,
         ARUCO_DICT)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
-
-objp = objp * squareLength_mm # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
-
-objp = objp * squareLength_mm
+objp = CHARUCO_BOARD.get
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Create the arrays and variables we'll use to store info like corners and IDs from images processed
-imgpoints = [] # Corners discovered in all images processed (3d point in real world space)
+corners_all = [] # Corners discovered in all images processed (3d point in real world space)
 objpoints = [] # 2d point in image plane
 ids_all = [] # Aruco ids corresponding to corners discovered
 image_size = None # Determined at runtime
@@ -48,30 +40,27 @@ dir_path = os.path.realpath(os.path.dirname(__file__))
 images = glob.glob(f'{dir_path}/../charuco_captures/*.jpg')
 
 # Loop through images glob'ed
-for iname in images:
+for im in images:
     # Open the image
-    img = cv2.imread(iname)
+    img = cv2.imread(im)
     # Grayscale the image
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Find aruco markers in the query image
-    detector = aruco.ArucoDetector(ARUCO_DICT, ARUCO_PARAM)
-    marker_corners, marker_ids, _ = detector.detectMarkers(gray)
+    # Detect marker corners
+    marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(gray, ARUCO_DICT)
 
     # Outline the aruco markers found in our query image
-    img = cv2.aruco.drawDetectedMarkers(img, marker_corners, borderColor = (0,255,0))
-
-    # Define ChArUco board object
-    CHARUCO_DETECTOR = cv2.aruco.CharucoDetector(CHARUCO_BOARD)
+    img = aruco.drawDetectedMarkers(img, marker_corners, borderColor = (0,255,0))
 
     # Get charuco corners and ids from detected aruco markers
-    charuco_corners, charuco_ids, marker_corners, marker_ids = CHARUCO_DETECTOR.detectBoard(image=gray)
+    _, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(marker_corners, marker_ids, gray, CHARUCO_BOARD)
 
     # If a Charuco board was found, let's collect image/corner points
     # Requiring at least 20 squares
     if len(charuco_ids) > 20:
         # Add these corners and ids to our calibration arrays
-        imgpoints.append(charuco_corners)
+        corners2 = cv2.cornerSubPix(gray, charuco_corners, (11,11), (-1,-1), criteria)
+        corners_all.append(corners2)
         ids_all.append(charuco_ids)
         objpoints.append(objp)
         
@@ -89,10 +78,12 @@ for iname in images:
         proportion = max(img.shape) / 1000.0
         img = cv2.resize(img, (int(img.shape[1]/proportion), int(img.shape[0]/proportion)))
         # Pause to display each image, waiting for key press
-        cv2.imshow(iname, img)
+        cv2.imshow(im, img)
+        cv2.waitKey(200)
+        cv2.destroyWindow(im)
         cv2.waitKey(10)
     else:
-        print("Not able to detect a charuco board in image: {}".format(iname))
+        print("Not able to detect a charuco board in image: {}".format(im))
     print("one is done")
 
 # Destroy any open CV windows
@@ -115,9 +106,9 @@ if not image_size:
 
 # Now that we've seen all of our images, perform the camera calibration
 # based on the set of points we've discovered
-calibration, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, image_size, None, None)
+calibration, cameraMatrix, distCoeffs, rvecs, tvecs = aruco.calibrateCameraCharuco(corners_all, ids_all, CHARUCO_BOARD, image_size, None, None)
 #calibration, cameraMatrix, distCoeffs, rvecs, tvecs = aruco.calibrateCameraAruco(
-#       charucoCorners=imgpoints,
+#       charucoCorners=corners_all,
 #        charucoIds=ids_all,
 #        board=CHARUCO_BOARD,
 #        imageSize=image_size,
@@ -134,4 +125,6 @@ print(distCoeffs)
 np.savez(f'{dir_path}/../charuco_calibration_values.npz', mtx=cameraMatrix, dist=distCoeffs)
     
 # Print to console our success
-print(f'Calibration Successful. Calibration calues saved in: {dir_path}/../charuco_calibration_values.npz')
+print(f'Calibration Successful. Calibration values saved in: {dir_path}/../charuco_calibration_values.npz')
+
+# Reprojection Error
